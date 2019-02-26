ee00f7387e2d1c6c5fded8bc08daccffa2362db6
Nicolas Susterman <nicosus95@hotmail.com>
2019-02-26 14:13:13 -0300

    Fix slider client spawn

---
+++ Overwacho/Assets/Scripts/EnableSlider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableSlider : MonoBehaviour {
+
+    [SerializeField] GameObject slider;
+
+    private void Start()
+    {
+        slider.SetActive(true);
+    }
+
+}
